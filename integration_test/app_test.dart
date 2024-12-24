965445bd98764d272c4c1ceb08c9de51e43fc1fd
mark <mark@orderly.network>
2024-12-24 20:45:15 +0800

    update integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,4 +1,4 @@
-import 'package:cicd_test/main.dart' as app;
+import 'package:cicd_test/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
@@ -9,16 +9,16 @@ void main() {
     testWidgets('tap on the floating action button, verify counter',
         (tester) async {
       // Load app widget.
-      await tester.pumpWidget(const app.MyApp());
+      await tester.pumpWidget(const MyApp());
 
       // Verify the counter starts at 0.
       expect(find.text('0'), findsOneWidget);
 
       // Finds the floating action button to tap on.
-      final fab = find.byKey(const ValueKey('increment'));
+      final incrementButtonFinder = find.byKey(const Key('increment'));
 
       // Emulate a tap on the floating action button.
-      await tester.tap(fab);
+      await tester.tap(incrementButtonFinder);
 
       // Trigger a frame.
       await tester.pumpAndSettle();
@@ -26,8 +26,16 @@ void main() {
       // Verify the counter increments by 1.
       expect(find.text('1'), findsOneWidget);
 
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
       // Emulate a tap on the floating action button.
-      await tester.tap(fab);
+      await tester.tap(incrementButtonFinder);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify the counter increments by 2.
       expect(find.text('2'), findsOneWidget);
     });
   });
